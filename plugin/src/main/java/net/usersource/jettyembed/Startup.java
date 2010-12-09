45393b7e48fd48ac99434eb964007769e55700f2
Glen Ford <glen.a.ford@gmail.com>
2010-12-09 20:41:19 +0000

    improve startup code add max timeout

--- plugin/src/main/java/net/usersource/jettyembed/Startup.java
+++ plugin/src/main/java/net/usersource/jettyembed/Startup.java
@@ -3,30 +3,38 @@ package net.usersource.jettyembed;
 import org.mortbay.jetty.Connector;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.bio.SocketConnector;
+import org.mortbay.jetty.webapp.WebAppClassLoader;
 import org.mortbay.jetty.webapp.WebAppContext;
 
 import java.net.URL;
 import java.security.ProtectionDomain;
 
+
 public class Startup {
 
     private static final int JETTY_PORT = 8080;
+    private static final int JETTY_MAX_IDLE = 30000;
 
     public static void main(String[] args) throws Exception {
-        Server server = new Server();
+
         SocketConnector connector = new SocketConnector();
         connector.setPort(Integer.getInteger("jettyPort", JETTY_PORT));
-        server.setConnectors(new Connector[]{connector});
-
-        WebAppContext context = new WebAppContext();
-        context.setServer(server);
-        context.setContextPath("/");
+        connector.setMaxIdleTime(Integer.getInteger("jettyMaxIdle", JETTY_MAX_IDLE));
 
         ProtectionDomain protectionDomain = Startup.class.getProtectionDomain();
         URL location = protectionDomain.getCodeSource().getLocation();
+
+        WebAppContext context = new WebAppContext();
+        context.setContextPath("/");
         context.setWar(location.toExternalForm());
 
-        server.addHandler(context);
+        WebAppClassLoader loader = new WebAppClassLoader( Startup.class.getClassLoader(), context );
+        context.setClassLoader(loader);
+
+        Server server = new Server();
+        server.setConnectors(new Connector[]{connector});
+        server.setHandler(context);
+        server.setSendServerVersion(false);
 
         try {
             server.start();
