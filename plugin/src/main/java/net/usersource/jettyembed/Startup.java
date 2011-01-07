fc0ed9ba0c90461de5392ffefbeb5d4c313634e0
glenford <glen.a.ford@gmail.com>
2011-01-07 12:01:24 +0000

    address issue #3

--- plugin/src/main/java/net/usersource/jettyembed/Startup.java
+++ plugin/src/main/java/net/usersource/jettyembed/Startup.java
@@ -7,6 +7,7 @@ import org.mortbay.jetty.webapp.WebAppClassLoader;
 import org.mortbay.jetty.webapp.WebAppContext;
 import org.mortbay.util.URIUtil;
 
+import java.io.File;
 import java.net.URL;
 import java.security.ProtectionDomain;
 
@@ -23,6 +24,8 @@ public class Startup {
         connector.setPort(Integer.getInteger("jettyPort", JETTY_PORT));
         connector.setMaxIdleTime(Integer.getInteger("jettyMaxIdle", JETTY_MAX_IDLE));
 
+        String tempDir = System.getProperty("jettyTempDir");
+
         Thread.currentThread().setContextClassLoader(WebAppClassLoader.class.getClassLoader());
 
         ProtectionDomain protectionDomain = Startup.class.getProtectionDomain();
@@ -33,6 +36,11 @@ public class Startup {
         context.setClassLoader(webAppClassLoader);
         context.setContextPath(URIUtil.SLASH);
         context.setWar(location.toExternalForm());
+
+        if( tempDir != null ) {
+            File tempDirectory = new File(tempDir);
+            context.setTempDirectory(tempDirectory);
+        }
 
         Server server = new Server();
         server.setConnectors(new Connector[]{connector});
